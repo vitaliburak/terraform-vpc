a3391bfb66f9fbb318e57a77b73a74d3bcfa5635
Vitalii Burak <72482900+vitaliburak@users.noreply.github.com>
2021-01-02 14:20:52 -0600

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
     backend "s3" {
-        bucket = "jenkins-terraform-evolvecybertraining"
+        bucket = "nowaytodelete-2020"
         key = "terraform_vpc/us-east-1/class/dev/infrastructure.tfstate"
         region = "us-east-1"
     }
